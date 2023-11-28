b333d03e2aebbaff7d579f15e54cc9fb233657b5
Yanik <wm_yanik@ukr.net>
2023-11-28 10:09:35 +0100

    item43 method reference types

--- lambdas_and_streams/item43_prefer_method_reference_to_lambda/src/main/java/org/example/Main.java
+++ lambdas_and_streams/item43_prefer_method_reference_to_lambda/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class Main {
   public static void main(String[] args) {
@@ -30,5 +34,62 @@ public class Main {
   //with method reference, much less verbose and more short and readable
   private static void merge2(Map<String, Integer> map, Integer value) {
     map.merge("asdf", value, Integer::sum);
+  }
+
+  // Five kinds of method references
+  //1. Static
+  //2. Bound
+  //3. Unbound
+  //4. Class Constructor
+  //5. Array Contructor
+  //Examples below
+
+
+  // Static
+  private static void staticReference() {
+    List.of("1")
+        .stream()
+        //Integer::valueOf is static method reference
+        //equivalent to: str -> Integer.valueOf(str)
+        .map(Integer::valueOf)
+        .toList();
+  }
+
+  // Bound
+  private static void boundReference() {
+    List.of(Instant.MIN)
+        .stream()
+        // Instant.now()::isAfter bound method reference
+        // equivalent to: final var instant = Instant.now(); t -> instant.isAfter(t);
+        .filter(Instant.now()::isAfter)
+        .toList();
+  }
+
+  // Unbound
+  private static void unboundReference() {
+    List.of("ASDF")
+        .stream()
+        // String::toLowerCase unbound method reference
+        // equivalent to str -> str.toLowerCase();
+        .map(String::toLowerCase)
+        .toList();
+  }
+
+  //Class Constructor
+  private static void classConstructorReference() {
+    List.of(List.of("asdf"))
+        //ArrayList<String>::new class constructor method reference
+        //equivalent to: (list) -> new ArrayList<String>(list);
+        .forEach(ArrayList<String>::new);
+  }
+
+  // Array constructor
+  private static void arrayConstructorReference() {
+    List.of(1)
+        .stream()
+        // array constructor reference
+        //equivalent to (len) -> new int[len];
+        .map(int[]::new)
+        .toList();
   }
 }
